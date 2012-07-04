098e14d8a25826ea4e545c424148aa741d7e4cd5
Hayden Muhl <haydenmuhl@yahoo.com>
2012-07-04 13:52:05 -0700

    WIP on Processor

--- src/com.haydenmuhl.nes/Processor.java
+++ src/com.haydenmuhl.nes/Processor.java
@@ -1,39 +1,58 @@
 package com.haydenmuhl.nes;
 
 public class Processor implements Clocked {
-    private IncrementRegister regPCL;
-    private IncrementRegister regPCH;
+    private static String MEMORY = "mem";
+
+    private IncrementRegister regPCH = new IncrementRegister((byte)0xff);
+    private IncrementRegister regPCL = new IncrementRegister((byte)0xfc);
     
-    private Memory memory;
+    private Mux<String, Byte> muxTemp = new Mux<String, Byte>();
+    private PullRegister<Byte> regTemp = new PullRegister<Byte>((byte)0);
     
-    private Instruction instruction;
-    private AddressingMode mode;
+    private DataSourceImpl<Byte, Memory> memory = new DataSourceImpl<Byte, Memory>();
+    
+    private MicroInstruction currentInstruction;
     
     public Processor() {
+        muxTemp.add(MEMORY, memory);
+        regTemp.setDataSource(muxTemp);
+        regPCL.setUpper(regPCH);
+
         reset();
     }
     
     public void reset() {
-        instruction = Instruction.JMP;
-        mode = AddressingMode.Absolute;
-        regPCH = new IncrementRegister((byte)0xff);
-        regPCL = new IncrementRegister((byte)0xfc);
+        regPCH.setValue((byte)0xff);
+        regPCL.setValue((byte)0xfc);
+        currentInstruction = JMP0;
     }
 
     public void tick() {
-    
+        currentInstruction.go();
+        currentInstruction = currentInstruction.next;
+        
+        regTemp.tick();
+        regPCL.tick();
+        regPCH.tick();
     }
     
-    public static enum Instruction {
-        JMP
+    public void setMemory(Memory mem) {
+        memory.setDataSource(mem);
     }
-
-    public static enum AddressingMode {
-        Absolute
-    }
+    
+    private final MicroInstruction JMP0 = this.new MicroInstruction() {
+        public void go() {
+            memory.getSource().setAddress(regPCH.output(), regPCL.output());
+            muxTemp.select(MEMORY);
+            regTemp.pull();
+            
+            regPCL.increment();
+        }
+    };
     
     private abstract class MicroInstruction {
         public abstract void go();
+        public MicroInstruction next;
     }
 }
 
