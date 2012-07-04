9dbeeaf36e4d440f98033b1820946dabc90568c6
Hayden Muhl <haydenmuhl@yahoo.com>
2012-07-04 13:51:44 -0700

    Tweak to DataSourceImpl

--- src/com.haydenmuhl.nes/DataSourceImpl.java
+++ src/com.haydenmuhl.nes/DataSourceImpl.java
@@ -1,14 +1,18 @@
 package com.haydenmuhl.nes;
 
-public class DataSourceImpl<T> implements DataSource<T> {
-    private DataSource<T> dataSource;
+public class DataSourceImpl<T, D extends DataSource<T>> implements DataSource<T> {
+    private D dataSource;
     
-    public void setDataSource(DataSource<T> source) {
+    public void setDataSource(D source) {
         dataSource = source;
         return;
     }
     
     public T output() {
         return dataSource.output();
+    }
+    
+    public D getSource() {
+        return dataSource;
     }
 }
