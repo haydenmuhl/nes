f376c45a6ee9935c5f3edc29f65dddf6f1503782
Hayden Muhl <haydenmuhl@yahoo.com>
2012-07-07 01:56:50 -0700

    Simple tests for MemoryImpl
    Gotta test that test code

---
+++ test/com.haydenmuhl.nes/MemoryImplTest.java
@@ -0,0 +1,25 @@
+package com.haydenmuhl.nes;
+
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+
+public class MemoryImplTest {
+    @Test
+    public void testSetByte() {
+        MemoryImpl mem = new MemoryImpl();
+        mem.memory[0x1234] = (byte) 0;
+        mem.setAddress((byte) 0x12, (byte) 0x34);
+        mem.setByte((byte) 0x42);
+        assertEquals(mem.memory[0x1234], (byte) 0x42);
+    }
+    
+    @Test
+    public void testGetByte() {
+        MemoryImpl mem = new MemoryImpl();
+        mem.memory[0x1234] = (byte) 0x42;
+        mem.setAddress((byte) 0x12, (byte) 0x34);
+        assertEquals(mem.getByte(), (byte) 0x42);
+        mem.memory[0x1234] = (byte) 0x99;
+        assertEquals(mem.getByte(), (byte) 0x99);
+    }
+}
